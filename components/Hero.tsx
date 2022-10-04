c585897edf68d9ccce336b975fc40dbb46baafbe
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-04 16:45:12 -0400

    cleanup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,14 +3,14 @@ import Image from "next/image"
 const Hero = () => {
 	return (
 		<div className='flex-grow flex items-center px-6 md:px-12'>
-			<div className='flex flex-col -mt-24 space-y-6'>
-				<span className='text-flatpurple font-header text-2xl'>
+			<div className='flex flex-col -mt-24 space-y-4'>
+				<span className='text-flatpurple font-header text-2xl -rotate-12 w-max'>
 					Hello!
 				</span>
 				<span className='font-header text-7xl'>
 					I am Ariel Okhtenberg
 				</span>
-				<span className='text-xl text-flatgray'>
+				<span className='text-xl font-light text-flatgray'>
 					React JS Developer & Graphic Designer
 				</span>
 			</div>
