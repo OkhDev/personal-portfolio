2d7d248aa98b48768866d2267bb48aba11f8fedf
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-04 16:12:49 -0400

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+		fontFamily: {
+			body: ["CoreSans", "sans-serif"],
+			header: ["Janna", "sans-serif"],
+		},
 		extend: {
 			colors: {
 				purple: "#8873EF",
 				offwhite: "#EDEDED",
 			},
-		},
-		fontFamily: {
-			body: ["CoreSans", "sans-serif"],
-			header: ["Janna", "sans-serif"],
 		},
 	},
 	plugins: [],
