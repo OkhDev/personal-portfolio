2b54023e1a94723750ba46b41ee91448fe0257ba
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-04 16:45:41 -0400

    cleanup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Hero = () => {
 	return (
 		<div className='flex-grow flex items-center px-6 md:px-12'>
-			<div className='flex flex-col -mt-24 space-y-4'>
+			<div className='flex flex-col -mt-36 space-y-4'>
 				<span className='text-flatpurple font-header text-2xl -rotate-12 w-max'>
 					Hello!
 				</span>
