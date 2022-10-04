c3f0ac4467d2c4b6d6e494c133f22ed7f8ae298d
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-04 16:05:20 -0400

    cleanup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 const Hero = () => {
 	return (
-		<div className='relative flex-grow flex justify-center items-center'>
+		<div className='relative flex-grow flex md:flex-row flex-col justify-center items-center'>
 			<div className='basis-1/2 font-header text-7xl'>
 				<span className='text-purple font-header'>Hello!</span>
 				<div className='font-header'>I am Ariel Okhtenberg</div>
 			</div>
-			<div className='basis-1/2 text-7xl'>Testing</div>
+			<div className='basis-1/2 font-header text-7xl'>Testing</div>
 		</div>
 	)
 }
