fd95f5d89ed0336bfab8a4de886489d3b235f039
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-05 14:26:32 -0400

    cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,9 @@ import Hero from "./Hero"
 import Navbar from "./Navbar"
 
 const Header = () => {
-	window.addEventListener("resize", () => {
-		let vh = window.innerHeight * 0.01
-		document.documentElement.style.setProperty("--vh", `${vh}px`)
-	})
-
 	return (
 		<>
-			<div className='flex flex-col screen-height'>
+			<div className='flex flex-col min-h-screen'>
 				<Navbar />
 				<Hero />
 			</div>
