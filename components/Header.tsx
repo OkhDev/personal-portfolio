59b6ffb70ffc822764f8f4aa2de2c2c059bcb0b8
aokhtenberg@gmail.com <111663408+OkhDev@users.noreply.github.com>
2022-10-05 14:17:00 -0400

    cleanup

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import Hero from "./Hero"
 import Navbar from "./Navbar"
 
 const Header = () => {
-	let vh = window.innerHeight * 0.01
-	document.documentElement.style.setProperty("--vh", `${vh}px`)
+	window.addEventListener("resize", () => {
+		let vh = window.innerHeight * 0.01
+		document.documentElement.style.setProperty("--vh", `${vh}px`)
+	})
 
 	return (
 		<>
